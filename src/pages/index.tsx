81bd971b1e7f9d06bbf244d7790fa32c428d3ed2
Anton Strogonoff <anton@strogonoff.name>
2020-08-17 15:23:02 +0900

    fix: link to docs from landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'components/linksButtons'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
     <h1>
-      Welcome to EXPRESS language reference!
+      Welcome to <Link to="docs">EXPRESS language reference!</Link>
     </h1>
   </div>
 )
